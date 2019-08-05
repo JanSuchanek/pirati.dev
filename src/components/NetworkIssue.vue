02cf02c672d2095b8c6931b5a22824fe84aa3fad
Jan Suchánek <jan.suchanek@gmail.com>
2019-08-05 13:50:34 +0200

    Smazání

--- src/components/NetworkIssue.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <h1>Uh-oh!</h1>
-    <h3>It looks like you're experiencing some network issues, please click the back button and try again.</h3>
-    <router-link :to="{ name: 'home' }">Or go back to the home page</router-link>
-  </div>
-</template>
