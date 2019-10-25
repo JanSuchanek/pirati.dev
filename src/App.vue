e4017f98c97cf8fada07413b386071753740321c
Jan Suchánek <jan.suchanek@gmail.com>
2019-10-25 11:37:49 +0200

    Oprava

--- src/App.vue
+++ src/App.vue
@@ -1,144 +1,32 @@
 <template>
   <div id="app">
-    <router-view />
-
-    <div class='footer'>
-        Vyhledávání napříč Pirátským ekosystémem. Ve stadiu ProofOfConcept, nestabilní. Agreguje data z fóra, redmine a proplácení. <br>
-        Licence: CC BY-SA | <a href='https://gitlab.com/Jarmil/pupik'>Zdrojový kód</a> | Připomínky na backend <a href='mailto:jarmil.halamicek@seznam.cz'>jarmil.halamicek@seznam.cz</a> nebo na frontend <a href='mailto:honza@suchanek.cz'>honza@suchanek.cz</a>
-        <img src='http://vps1.pir-test.eu:8000/assets/cc-by-sa.png' class='license-image'>
-    </div>    
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-
-  color: #00;
+  text-align: center;
+  color: #2c3e50;
 }
 
-
-html {
-    font-family: sans-serif;
+#nav {
+  padding: 30px;
 }
 
-.home {
-    width: 75%;
-    margin-left: auto;
-    margin-right: auto;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-.searchbox-area {
-    margin-top: 50px;
-}
-
-.search-title-name {
-    font-weight: normal;
-    font-size: 60px;
-    /*+placement: 2px -8px;*/
-    position: relative;
-    left: 2px;
-    top: -8px;
-}
-
-.search-phrase input {
-    font-size: 20px;
-    /*+border-radius: 70px;*/
-    -moz-border-radius: 70px;
-    -webkit-border-radius: 70px;
-    -khtml-border-radius: 70px;
-    border-radius: 70px;
-    background-color: #FFFFFF;
-    border: 1px solid #C1C1C1;
-    padding-top: 9px;
-    padding-bottom: 8px;
-    padding-left: 28px;
-    width: 50%;
-}
-
-.search-button button {
-    color: #FFFFFF;
-    background-color: #000000;
-    /*+border-radius: 875px;*/
-    -moz-border-radius: 875px;
-    -webkit-border-radius: 875px;
-    -khtml-border-radius: 875px;
-    border-radius: 875px;
-    border: 0px solid black;
-    padding: 9px 27px;
-    font-size: 20px;
-}
-
-.results-count {
-    margin-top: 16px;
-    font-size: 90%;
-    color: #777777;
-    margin-left: 21px;
-}
-
-.result {
-    margin-top: 19px;
-    margin-bottom: 20px;
-    margin-left: 21px;
-}
-
-.result-title {
-    font-weight: 600;
-    font-size: 110%;
-}
-
-.result-link a {
-    color: #0087F7;
-}
-
-.result-snipplet {
-    color: #595959;
-}
-
-.footer {
-    background-color: #383737;
-    padding: 17px 39px 40px 40px;
-    color: #FFFFFF;
-    margin-top: 88px;
-}
-
-.license-image {
-    float: left;
-    margin-right: 15px;
-    margin-top: 5px;
-}
-
-.footer a {
-    color: #C7C7C7;
-}
-
-.hidden {
-    display: none;
-}
-
-.search-title-logo-image {
-    width: 56px;
-    margin-left: 6px;
-}
-
-.filter-row {
-    margin-top: 20px;
-}
-
-.filter-checkbox {
-    border: 1px solid #949494;
-    margin-right: 9px;
-    /*+border-radius: 50px;*/
-    -moz-border-radius: 50px;
-    -webkit-border-radius: 50px;
-    -khtml-border-radius: 50px;
-    border-radius: 50px;
-    padding: 6px 13px 6px 6px;
-}
-
-.filter_checked {
-    font-weight: bold;
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,58 +1,18 @@
 <template>
-	<div class="home">
-		<div class='search-title'>
-			<span class='search-title-logo'><img src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Logo_Pirátů.svg' class='search-title-logo-image' alt="Pirati"></span>
-			<span class='search-title-name'>PUPÍK</span>
-		</div>
-		<Search @change="changeQuery" />
-		<List :items="items" />        
-	</div>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-
-import Search from "@/components/Search.vue";
-import List from "@/components/List.vue";
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-	name: "home",
-	data() {
-		return {
-			items: [],
-			url: 'http://vps1.pir-test.eu:9200/article/_search'
-		}
-	},
-	components: {
-		Search, List
-	},
-	methods: {
-		changeQuery(queryData) {
-
-			if(!queryData.query){
-				return null
-			}
-			try {                   
-				fetch(this.url, {
-						method: 'POST',
-						headers: {
-								'Content-Type': 'application/json',
-						},
-						body: JSON.stringify(queryData),
-				})
-					.then(response => {
-						return response.json()
-					})
-					.then(data => {
-						if(data["hits"]){
-							this.items = data["hits"]["hits"]
-						}
-
-				})
-			} catch (e) {
-					this.errors.push(e)
-			}
-		}  
-	}
+  name: 'home',
+  components: {
+    HelloWorld
+  }
 }
-
 </script>
